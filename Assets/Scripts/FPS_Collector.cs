97203190b1874177b1ab43aaef81dc8a9dc8ff8c
Bozodo <aquinojt.1@gmail.com>
2020-05-11 18:33:28 -0400

    Created workling pickups

---
+++ Assets/Scripts/FPS_Collector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FPS_Collector : MonoBehaviour
+{
+    
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
