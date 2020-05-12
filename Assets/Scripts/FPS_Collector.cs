606669453058884ec385419b91d2ca1f633e7587
Bozodo <aquinojt.1@gmail.com>
2020-05-11 20:07:22 -0400

    Doubled the FPS controller run speed and added basic UI

--- Assets/Scripts/FPS_Collector.cs
+++ Assets/Scripts/FPS_Collector.cs
@@ -9,23 +9,37 @@ public class FPS_Collector : MonoBehaviour
     public Text countText;
     public Text winText;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pickup"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-        }
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 3)
+        {
+            winText.text = "Close Enough";
+        }
     }
 }
